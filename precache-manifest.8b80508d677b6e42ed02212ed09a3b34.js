f02aad502b06b34321576c88632977ebca1b920a
dario <darioh.dev@gmail.com>
2020-04-23 14:01:23 -0300

    Updates

---
+++ precache-manifest.8b80508d677b6e42ed02212ed09a3b34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "290cc6600b6dadd265765376c4c9d095",
+    "url": "/React-coin-flipper/index.html"
+  },
+  {
+    "revision": "6cdf5b181350eec85d3a",
+    "url": "/React-coin-flipper/static/css/main.b6349204.chunk.css"
+  },
+  {
+    "revision": "01a6bfa2ae7284624d8f",
+    "url": "/React-coin-flipper/static/js/2.c86959d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-coin-flipper/static/js/2.c86959d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cdf5b181350eec85d3a",
+    "url": "/React-coin-flipper/static/js/main.3dd6d2dc.chunk.js"
+  },
+  {
+    "revision": "2429b51189982ee5fb43",
+    "url": "/React-coin-flipper/static/js/runtime-main.37df3c30.js"
+  }
+]);
